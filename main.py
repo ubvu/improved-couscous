a3a93fbd7647b02c699266d7b6358f5c105f7c1f
marcel-blegh <m.r.jouvenaz@vu.nl>
2024-07-15 17:19:42 +0200

    bagger, forgot to work in a branch

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ era = project_scope.era
 institutions = project_scope.institution_list
 nice_list = project_scope.NICE_LIST
 file_name = 'test_csv.csv'
+field_names = project_scope.data_fields
 
 
 def query_year(my_year):
     global page_number, inst
 
     api_url = ("https://api.openalex.org/works?page=" + str(page_number) +
-               "&filter=authorships.institutions.lineage:i865915315,publication_year:2022,"
+               "&filter=authorships.institutions.lineage:i865915315,publication_year:" + str(my_year) + ","
                "type:types/article&select=id,topics,doi,ids,publication_year,fwci,type,cited_by_count,"
                "cited_by_percentile_year,open_access,sustainable_development_goals,counts_by_year"
                "&mailto=" + str(nice_list))
@@ -29,8 +30,19 @@ def unpack(my_json, this_year):
     my_meta = pd.DataFrame.from_dict(pd.json_normalize(my_json['meta']))
     if int(my_meta['page'][0]) * int(my_meta["per_page"][0]) >= int(my_meta['count'][0]):
         keep_at_it = False
-    my_results = pd.DataFrame.from_dict(pd.json_normalize(my_json['results']), orient='columns')
-    my_results.to_csv(file_name, index=False, mode='a', header=not os.path.exists(file_name))
+    my_data = my_json["results"]
+    for result in my_data:
+        global field_names
+        for field_name in field_names:
+            try:
+                my_id = result[field_name]
+                print(my_id)
+                # now work on cleaning the data and save into file
+            except Exception:
+                continue
+
+    # my_results = pd.DataFrame.from_dict(pd.json_normalize(my_json['results']), orient='columns')
+    # my_results.to_csv(file_name, index=False, mode='a', header=not os.path.exists(file_name))
 
 
 print(era)
@@ -42,5 +54,6 @@ for inst in institutions:
         while keep_at_it:
             this_json = query_year(year)
             unpack(this_json, year)
+            #  += 100 is obviously a test setting
             page_number += 100
             print(page_number)
