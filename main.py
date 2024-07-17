a2ac262b77144e32badfc365a13ba3c571e8548d
marcel-blegh <m.r.jouvenaz@vu.nl>
2024-07-17 15:00:43 +0200

    not what we want, this doesn't allow us to select deeper json-fields automatically. The rest works though

--- main.py
+++ main.py
@@ -14,41 +14,47 @@ field_names = project_scope.data_fields
 
 def query_year(my_year):
     global page_number, inst
-
-    api_url = ("https://api.openalex.org/works?page=" + str(page_number) +
-               "&filter=authorships.institutions.lineage:i865915315,publication_year:" + str(my_year) + ","
-               "type:types/article&select=id,topics,doi,ids,publication_year,fwci,type,cited_by_count,"
-               "cited_by_percentile_year,open_access,sustainable_development_goals,counts_by_year"
-               "&mailto=" + str(nice_list))
-    response = requests.get(api_url)
+    params = {
+        'filter': f'publication_year:{str(my_year)},authorships.institutions.lineage:{"i865915315"},type:types/article',
+        'select': 'id,ids,doi,topics,publication_year,fwci,type,cited_by_count,cited_by_percentile_year,'
+                  'authorships,open_access,sustainable_development_goals,counts_by_year',
+        'page': str(page_number),
+        'mailto': str(nice_list)
+    }
+    api_url = "https://api.openalex.org/works"
+    response = requests.get(api_url, params=params)
     return response.json()
 
 
 def unpack(my_json, this_year):
-    global file_name, keep_at_it
+    global file_name, keep_at_it, field_names
+
     file_name = str(".\\output\\") + str(this_year) + ".csv"
-    my_meta = pd.DataFrame.from_dict(pd.json_normalize(my_json['meta']))
-    if int(my_meta['page'][0]) * int(my_meta["per_page"][0]) >= int(my_meta['count'][0]):
-        keep_at_it = False
-    my_data = my_json["results"]
-    for result in my_data:
-        global field_names
-        for field_name in field_names:
+    keep_at_it = (int(my_json['meta']['page']) * int(my_json['meta']["per_page"]) < int(my_json['meta']['count']))
+
+    for result in my_json["results"]:
+        line_data = pd.Series()
+        for key, val in field_names.items():
             try:
-                my_id = result[field_name]
-                print(my_id)
-                # now work on cleaning the data and save into file
-            except Exception:
+                print(field_names)
+                print(f"key {key} \n")
+                print(f"val {val} \n")
+                line_data.loc[key] = result[val]
+            except json.JSONDecodeError:
+                line_data.loc[key] = "[]"
                 continue
+        if 'dataframe' not in globals():
+            global dataframe
+            dataframe = line_data.to_frame().T
+        else:
+            dataframe = pd.concat([dataframe, line_data.to_frame().T], ignore_index=True)
+    if "dataframe" in globals():
+        dataframe.to_csv(file_name, index=False, mode='a', header=not os.path.exists(file_name))
+        del dataframe
 
-    # my_results = pd.DataFrame.from_dict(pd.json_normalize(my_json['results']), orient='columns')
-    # my_results.to_csv(file_name, index=False, mode='a', header=not os.path.exists(file_name))
 
-
-print(era)
 for inst in institutions:
     for year in era:
-        print(year)
         page_number = 1
         keep_at_it = True
         while keep_at_it:
--- scope_file.py
+++ scope_file.py
@@ -1,5 +1,5 @@
 # this should be overwritten with your real project data
-# which obviously shouldn't be on github.
+# which obviously shouldn't be on gitHub.
 # Obey the captain or learn to swim !
 
 era = range(2015, 2016, 1)      # last year not included
@@ -8,4 +8,7 @@ institution_list = ["i865915315}"]
 # , "i887064364", "i121797337", "i98358874", "i913958620", "i193700539", "i83019370",
 # "i34352273", "i145872427", "i913481162", "i193662353", "i94624287", "i169381384"]
 NICE_LIST = "myname@example.com"
-data_fields = {"id": "id"}
+
+# vergadering met UNL om te bepalen welke velden in de data_fields dict moeten staan
+# deze dict bepaalt welke velden uit de json naar de csv worden opgeslagen.
+data_fields = {"id": "id", "doi": "doi", "open_access": "open_access", "ids": "ids"}
