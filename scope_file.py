f568dbda19303dcd61094d8e49f148091d4e98ed
marcel-blegh <m.r.jouvenaz@vu.nl>
2024-07-17 17:03:51 +0200

    Still not picking the right data

--- scope_file.py
+++ scope_file.py
@@ -3,7 +3,7 @@
 # Obey the captain or learn to swim !
 
 era = range(2015, 2016, 1)      # last year not included
-institution_set = {"Vrije Universiteit": "i865915315"}
+institution_dict = {"Vrije Universiteit": "i865915315"}
 institution_list = ["i865915315}"]
 # , "i887064364", "i121797337", "i98358874", "i913958620", "i193700539", "i83019370",
 # "i34352273", "i145872427", "i913481162", "i193662353", "i94624287", "i169381384"]
@@ -11,4 +11,4 @@ NICE_LIST = "myname@example.com"
 
 # vergadering met UNL om te bepalen welke velden in de data_fields dict moeten staan
 # deze dict bepaalt welke velden uit de json naar de csv worden opgeslagen.
-data_fields = {"id": "id", "doi": "doi", "open_access": "open_access", "ids": "ids"}
+data_fields = ["id", "doi", "open_access", "ids"]
