3e6c037a4aa6115268ccceb975cab8eeeea0f038
marcel-blegh <m.r.jouvenaz@vu.nl>
2024-07-29 16:41:53 +0200

    data cleaning, powerBI doesn't quite work as I expected.

--- institute_cleaner.py
+++ institute_cleaner.py
@@ -2,6 +2,9 @@ import os
 import ast
 import pandas as pd
 
+import scope_file as project_scope
+uni_list = project_scope.institution_dict
+
 path = './output'
 
 
@@ -10,31 +13,31 @@ def find_main_institute(json_string):
     for thingamadoodle in ast.literal_eval(json_string):
         match (thingamadoodle['id'].split("/"))[-1]:
             case "I865915315" | "I4210145651" | "I4210108594" | "I4210124285" | "I911458345" | "I4210153566":
-                collector.append("vrije universiteit")
+                collector.append("Vrije Universiteit")
             case "I887064364" | "I4210145651" | "I4210109446" | "I4210108594" | "I4210114434" | "I4210153566" | "I2802928900":
-                collector.append("universiteit van amsterdam")
+                collector.append("Universiteit van Amsterdam")
             case "I121797337" | "I4210114434" | "I2800006345":
-                collector.append("universiteit leiden")
+                collector.append("Universiteit Leiden")
             case "I913958620" | "I2801952686" | "I4210142620":
-                collector.append("erasmus universiteit rotterdam")
+                collector.append("Erasmus Universiteit Rotterdam")
             case "I98358874" | "I4210145640" | "I4210099110":
-                collector.append("technische universiteit delft")
+                collector.append("Technische Universiteit Delft")
             case "I193700539" | "I124486421":
-                collector.append("tilburg universiteit")
+                collector.append("Universiteit van Tilburg")
             case "I83019370" | "I124486421":
-                collector.append("technische universiteit eindhoven")
+                collector.append("Technische Universiteit Eindhoven")
             case "I34352273" | "I2801238018" | "I2800191616" | "I4210126394":
-                collector.append("universiteit maastricht")
+                collector.append("Universiteit Maastricht")
             case "i145872427" | "I2801238018" | "I4210126394" | "I2802934949":
-                collector.append("radboud universiteit nijmegen")
+                collector.append("Radboud Universiteit Nijmegen")
             case "I913481162":
-                collector.append("wageningen universiteit")
+                collector.append("Wageningen Universiteit")
             case "I193662353" | "I4210114434" | "I4210107283" | "I3018483916":
-                collector.append("universiteit utrecht")
+                collector.append("Universiteit Utrecht")
             case "I94624287" | "I4210122839":
-                collector.append("universiteit twente")
+                collector.append("Universiteit Twente")
             case "I169381384" | "I4210154073" | "I1334415907":
-                collector.append("rijksuniversiteit groningen")
+                collector.append("Rijksuniversiteit Groningen")
             case _:
                 pass
     return set(collector)
@@ -43,6 +46,8 @@ def find_main_institute(json_string):
 for filename in os.listdir(path):
     f = os.path.join(path, filename)
     if os.path.isfile(f):
-        df = pd.read_csv(f)
+        df = pd.read_csv(f, index_col=False)
         df['authorships'] = df['authorships'].apply(find_main_institute)
-        df.to_csv(".\\clean\\" + f)
+        for key, value in uni_list.items():
+            df[key] = df['authorships'].apply(lambda x: key in x)
+        df.to_csv(".\\clean\\" + f, index=False)
